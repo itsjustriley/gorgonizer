315a6b35ca19d6f0a89e49b947c5310212770d53
Riley Nowak <rileynowak@Mac.home>
2025-10-05 10:12:39 -0300

    add verbose and defer output flags

--- gorgonizer.go
+++ gorgonizer.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"path/filepath"
 	"github.com/h2non/filetype"
 )
 
@@ -24,9 +26,37 @@ func getFileClass(buf []byte) string {
 	}
 }
 
+func isClassFolder(name string) bool {
+    switch name {
+    case "Images", "Videos", "Audio", "Archives", "Documents", "Other":
+        return true
+    default:
+        return false
+    }
+}
+
+var verbose bool
+var deferOutput bool
+var outputMessages []string
+
+func printAndDeferOutput(message string) {
+    if verbose {
+        fmt.Println(message)
+    }
+    if deferOutput {
+        outputMessages = append(outputMessages, message)
+    }
+}
+
+func printOutputMessages() {
+    for _, message := range outputMessages {
+        fmt.Println(message)
+    }
+}
+
 func organizeFile(directory string, file os.FileInfo, includeSubfolders bool) {
     if file.IsDir() {
-        if includeSubfolders {
+        if includeSubfolders && !isClassFolder(file.Name()) {
             organizeDirectory(directory+"/"+file.Name(), includeSubfolders)
         }
         return
@@ -35,23 +65,28 @@ func organizeFile(directory string, file os.FileInfo, includeSubfolders bool) {
     filePath := directory + "/" + file.Name()
     buf, err := ioutil.ReadFile(filePath)
     if err != nil {
-        fmt.Printf("Error reading file %s: %v\n", file.Name(), err)
+        printAndDeferOutput("Error reading file " + file.Name() + ": " + err.Error())
         return
     }
 
     if len(buf) == 0 {
-        fmt.Printf("Skipping empty file: %s\n", file.Name())
+        printAndDeferOutput("Skipping empty file: " + file.Name())
         return
     }
 
     fileClass := getFileClass(buf)
+
+    if filepath.Base(directory) == fileClass {
+        return
+    }
+
     subfolder := directory + "/" + fileClass
     if _, err := os.Stat(subfolder); os.IsNotExist(err) {
         os.Mkdir(subfolder, 0755)
     }
 
     if err := os.Rename(filePath, subfolder+"/"+file.Name()); err != nil {
-        fmt.Printf("Error moving file %s: %v\n", file.Name(), err)
+        printAndDeferOutput("Error moving file " + file.Name() + ": " + err.Error())
     }
 }
 
@@ -60,8 +95,8 @@ func organizeDirectory(directory string, includeSubfolders bool) (int, error) {
 	if err != nil {
 			return 0, err
 	}
-	fmt.Println("Organizing directory:", directory)
-  fmt.Println("Found", len(files), "entries in:", directory)
+	printAndDeferOutput("Organizing directory: " + directory)
+  printAndDeferOutput("Found " + strconv.Itoa(len(files)) + " entries in: " + directory)
 	for _, file := range files {
 			organizeFile(directory, file, includeSubfolders)
 	}
@@ -72,6 +107,8 @@ func main() {
 	// TODO: implement flag behaviour
 	directory := flag.String("dir", "sample-folder", "The directory to organize.")
 	includeSubfolders := flag.Bool("include-subfolders", false, "Include subfolders in the organization.")
+  flag.BoolVar(&verbose, "verbose", false, "Print verbose output.")
+  flag.BoolVar(&deferOutput, "defer-output", false, "Defer output until the end.")
 	// log := flag.Bool("log", false, "Save a log of operations.")
 	// exactMatch := flag.Bool("exact", false, "Organize by exact type ONLY.")
 	flag.Parse()
@@ -81,13 +118,19 @@ func main() {
 		return
 	}
 
-    count, err := organizeDirectory(*directory, *includeSubfolders)
-    if err != nil {
-        fmt.Println("Error: Failed to read directory.")
-        return
-    }
+	fmt.Println("Starting organization...")
 
-    fmt.Println("Found", count, "files in directory.")
+	count, err := organizeDirectory(*directory, *includeSubfolders)
+	if err != nil {
+			printAndDeferOutput("Error: Failed to read directory " + *directory + ": " + err.Error())
+			return
+	}
+
+	printAndDeferOutput("Found " + strconv.Itoa(count) + " files in directory " + *directory)
 
 	fmt.Println("Organization complete.")
+	if deferOutput {
+		fmt.Println("--------------------------------")
+		printOutputMessages()
+	}
 }
