a88382447bb6dd2bb3824ecf9ff18e03f1fe47b7
Riley Nowak <rileynowak@Mac.home>
2025-10-08 12:00:50 -0300

    go fmt

--- gorgonizer_test.go
+++ gorgonizer_test.go
@@ -5,50 +5,49 @@ import (
 )
 
 func TestIsExactFolder(t *testing.T) {
-    var tests = []struct {
-        name     string
-        expected bool
-    }{
-        {"NoExt", true},
-        {"PNG", true},
-        {"MOV", true},
-        {"Images", false},
-        {"MY MOV", false},
-        {"", false},
-    }
+	var tests = []struct {
+		name     string
+		expected bool
+	}{
+		{"NoExt", true},
+		{"PNG", true},
+		{"MOV", true},
+		{"Images", false},
+		{"MY MOV", false},
+		{"", false},
+	}
 
-    for _, test := range tests {
-        t.Run(test.name, func(t *testing.T) {
-            if output := isExactFolder(test.name); output != test.expected {
-                t.Errorf("input %q, expected %v, got %v", test.name, test.expected, output)
-            }
-        })
-    }
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if output := isExactFolder(test.name); output != test.expected {
+				t.Errorf("input %q, expected %v, got %v", test.name, test.expected, output)
+			}
+		})
+	}
 }
 
-
 func TestHumanizeBytes(t *testing.T) {
-    var tests = []struct {
-        size     int64
-        expected string
-    }{
-        {1023, "1023 B"},
-        {1024, "1.00 KB"},
-        {1536, "1.50 KB"},
-        {1024 * 1024, "1.00 MB"},
-        {1024 * 1024 * 1024, "1.00 GB"},
-    }
-    for _, test := range tests {
-        t.Run(test.expected, func(t *testing.T) {
-            if output := humanizeBytes(test.size); output != test.expected {
-                t.Errorf("input %d, expected %q, got %q", test.size, test.expected, output)
-            }
-        })
-    }
+	var tests = []struct {
+		size     int64
+		expected string
+	}{
+		{1023, "1023 B"},
+		{1024, "1.00 KB"},
+		{1536, "1.50 KB"},
+		{1024 * 1024, "1.00 MB"},
+		{1024 * 1024 * 1024, "1.00 GB"},
+	}
+	for _, test := range tests {
+		t.Run(test.expected, func(t *testing.T) {
+			if output := humanizeBytes(test.size); output != test.expected {
+				t.Errorf("input %d, expected %q, got %q", test.size, test.expected, output)
+			}
+		})
+	}
 }
 
 func BenchmarkHumanizeBytes(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        humanizeBytes(1 << 40)
-    }
+	for i := 0; i < b.N; i++ {
+		humanizeBytes(1 << 40)
+	}
 }
